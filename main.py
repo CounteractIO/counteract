93c036ac9918b4e8de390d91ee6a1cb594be39bc
Jay Shenoy <jayshenoy@hotmail.com>
2016-08-04 20:00:20 -0400

    Implement tweet model and display bubbles on map

--- main.py
+++ main.py
@@ -2,56 +2,16 @@ from flask import Flask, render_template, request, url_for, redirect, make_respo
 from flask.ext.login import LoginManager, current_user, login_user, logout_user, login_required
 import jinja2
 
-import hashlib
-import random
-import string
-import os
-
-from mongoengine import *
+from mongoengine import connect
+from models import User, Tweet, create_user
 
 app = Flask(__name__)
 app.config.from_object('config')
-
-class User(Document):
-	name = StringField(required=True, max_length=100)
-	username = StringField(required=True)
-	password = StringField(required=True)
-	salt = StringField(required=True)
-
-	def is_authenticated(self):
-		return True
-
-	def is_active(self):
-		return True
-
-	def is_anonymous(self):
-		return False
-
-	def get_id(self):
-		return self.username
-
-	def check_password_hash(self, form_password):
-		m = hashlib.sha512()
-		m.update(self.salt)
-		m.update(form_password)
-		return self.password == m.hexdigest()
 
 # Login management
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
-
-def create_user(name, username, password):
-	# Generate random salt
-	salt = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for i in range(0, 15))
-
-	# Generate password hash
-	m = hashlib.sha512()
-	m.update(salt)
-	m.update(password)
-	password_hash = m.hexdigest()
-
-	return User(name=name, username=username, password=password_hash, salt=salt)
 
 @login_manager.user_loader
 def load_user(userid):
