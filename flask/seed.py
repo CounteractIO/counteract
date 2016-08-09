cc9bdddc7ed5d98b21b09c8d7c9d2b25b0379d36
Jay Shenoy <jayshenoy@hotmail.com>
2016-08-08 22:39:11 -0400

    Implement Twitter tracker

--- flask/seed.py
+++ flask/seed.py
@@ -13,7 +13,8 @@ from config import MONGODB_NAME, MONGODB_URI
 connect(MONGODB_NAME, host=MONGODB_URI)
 
 def seed_tweets():
-    tweets = []
+    # Delete all previously generated tweets
+    Tweet.objects().delete()
 
     # Generate 100 random tweets
     for i in range(1000):
@@ -26,7 +27,6 @@ def seed_tweets():
         end_date = today.toordinal()
         date = datetime.fromordinal(random.randint(start_date, end_date))
 
-        num_likes = random.randint(0, 200)
         num_retweets = random.randint(0, 50)
 
         location = [random.uniform(-180, 180), random.uniform(-90, 90)]
@@ -35,12 +35,7 @@ def seed_tweets():
 
         risk_level = random.uniform(0, 100)
 
-        tweet = Tweet(name=name, handle=handle, date=date, num_likes=num_likes, num_retweets=num_retweets, location=location, content=content, risk_level=risk_level)
+        tweet = Tweet(name=name, handle=handle, date=date, num_retweets=num_retweets, location=location, content=content, risk_level=risk_level)
         tweet.save()
-        #tweets.append(json.loads(tweet.to_json()))
-
-    '''f = open('static/js/tweets.json', 'w')
-    f.write(json.dumps(tweets))
-    f.close()'''
 
 seed_tweets()
