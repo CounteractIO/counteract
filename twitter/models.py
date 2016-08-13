0d07ba1f818865d282b1bf70b38eefac5efd0b78
Jay Shenoy <jayshenoy@hotmail.com>
2016-08-12 20:46:34 -0400

    Seed database with land-based coordinates

--- twitter/models.py
+++ twitter/models.py
@@ -3,7 +3,7 @@ from mongoengine import *
 class Tweet(Document):
 	name = StringField(required=True)
 	handle = StringField(required=True)
-	date = DateTimeField(required=True)
+	date = LongField(required=True)
 	num_retweets = IntField(required=True)
 	location = PointField(required=True, null=True)
 	content = StringField(required=True)
--- twitter/seed.py
+++ twitter/seed.py
@@ -16,8 +16,8 @@ def seed_tweets():
     # Delete all previously generated tweets
     Tweet.objects().delete()
 
-    # Generate 1000 random tweets
-    for i in range(1000):
+    # Generate 500 random tweets
+    for i in range(500):
         name = ''.join(random.SystemRandom().choice(string.ascii_lowercase) for _ in range(6)) + ' ' + ''.join(random.SystemRandom().choice(string.ascii_lowercase) for _ in range(6))
         handle = ''.join(random.SystemRandom().choice(string.ascii_lowercase) for _ in range(8))
 
@@ -28,7 +28,13 @@ def seed_tweets():
         date = int(datetime.fromordinal(random.randint(start_date, end_date)).strftime('%s')) * 1000
         num_retweets = random.randint(0, 50)
 
-        location = [random.uniform(-180, 180), random.uniform(-90, 90)]
+        # Only choose coordinates located on land
+        america = [random.uniform(-120, -75), random.uniform(30, 60)]
+        south_america = [random.uniform(-75, -60), random.uniform(-45, 0)]
+        africa = [random.uniform(15, 30), random.uniform(-30, 30)]
+        europe = [random.uniform(-10, 30), random.uniform(40, 55)]
+        asia = [random.uniform(30, 135), random.uniform(23, 67)]
+        location = random.choice([america, south_america, africa, europe, asia])
 
         content = ''.join(random.SystemRandom().choice(string.ascii_lowercase) for _ in range(140))
 
