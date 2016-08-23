cdad082fed60ee2f59a5c6dad7093f6281a9e662
Andy Kamath <a@andykamath.com>
2016-08-23 16:06:55 -0400

    swag

--- meteor/main.js
+++ meteor/main.js
@@ -2,8 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import Heap from './heap.js'
 
 tweets = new Mongo.Collection('tweet')
+tweet = new Mongo.Collection('tweets')
 if (Meteor.isClient) {
-    
     Template.body.onRendered(function() {
         tweetstream = []
         tweets.find().observe({
@@ -12,6 +12,15 @@ if (Meteor.isClient) {
             }
         })
         console.log(tweetstream)
+    });
+    Template.body.helpers({
+        main: function() {
+            if (ActiveRoute.path('/')) return 'home'
+            else return 'vis'
+        },
+        tweet: function() {
+            return tweet.find().fetch()
+        }
     })
 }
 if (Meteor.isServer) {
@@ -29,8 +38,21 @@ if (Meteor.isServer) {
     if (tweets.find().count() == 0) {
         tweets.insert({ "name": "ultybs scjhkv", "handle": "zqwvteop", "date": 1470715200000, "num_retweets": 28, "location": { "type": "Point", "coordinates": [-168.11289992853986, 40.92125425551876] }, "content": "miubkotohagiqjurbnydwchbgigpzznkbhlccvvwfbgmwtzebwbzmqysovcjxzburhfmeckrvyomkfmnkeoirnpotmbzydnxcigvkmukornvgiewyhkozuirqshklorwpjssoblztuvr", "risk_level": 86 });
     }
-
     Meteor.publish("tweet", function() {
         return tweets.find();
+    });
+    tweet.allow({
+        insert: function() {
+            return true;
+        },
+        update: function() {
+            return true;
+        },
+        remove: function() {
+            return true;
+        }
+    });
+    Meteor.publish("tweets", function() {
+        return tweet.find();
     });
 }
