c594b8f6771d91ae45d786e70f5c1e46bf93f2ed
yashLadha <yashladhapankajladha123@gmail.com>
2016-12-24 23:01:16 +0530

    Formatted to PEP8 format

--- index.py
+++ index.py
@@ -13,7 +13,7 @@ from tweepy.streaming import StreamListener
 
 auth = tweepy.auth.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
- 
+
 api = tweepy.API(auth)
 
 traintweets = []
@@ -37,7 +37,7 @@ with open('classify.txt') as f:
 
 # Create feature vectors
 vectorizer = TfidfVectorizer(min_df=5,
-                             max_df = 0.8,
+                             max_df=0.8,
                              sublinear_tf=True,
                              use_idf=True)
 trainvec = vectorizer.fit_transform(traintweets)
@@ -50,10 +50,10 @@ classifier_liblinear.fit(trainvec, trainclass)
 t1 = time.time()
 prediction_liblinear = classifier_liblinear.predict(testvec)
 t2 = time.time()
-time_liblinear_train = t1-t0
-time_liblinear_predict = t2-t1
+time_liblinear_train = t1 - t0
+time_liblinear_predict = t2 - t1
 
-#Train Markov Chain
+# Train Markov Chain
 with open('encouraging.txt') as f:
     text = f.read()
 text_model = markovify.Text(text)
@@ -61,24 +61,26 @@ text_model = markovify.Text(text)
 print "Training time: %fs; Prediction time: %fs" % (time_liblinear_train, time_liblinear_predict)
 print classification_report(testclass, prediction_liblinear)
 
+
 class MyListener(StreamListener):
- 
     def on_data(self, data):
         try:
-            new = json.loads(data)['text'].replace('RT ','')
+            new = json.loads(data)['text'].replace('RT ', '')
             testvec = vectorizer.transform([new])
             prediction_liblinear = classifier_liblinear.predict(testvec)
-            if('s' in prediction_liblinear[0]):
+
+            if 's' in prediction_liblinear[0]:
                 user = json.loads(data)['user']['screen_name']
-                api.update_status("@"+user+" "+text_model.make_short_sentence(138 - len(user)))
+                api.update_status("@" + user + " " + text_model.make_short_sentence(138 - len(user)))
 
         except BaseException as e:
             print('&quot;Error on_data: %s&quot;' % str(e))
         return True
- 
+
     def on_error(self, status):
         print(status)
         return True
- 
+
+
 twitter_stream = Stream(auth, MyListener())
 twitter_stream.filter(track=['anxiety', 'sadness', 'suicide', 'depression', 'sad'])
--- nlp1.py
+++ nlp1.py
@@ -1,83 +1,112 @@
-#nlp test
+# nlp test
 import numpy
 from sklearn import svm
 from sklearn import datasets
-#returns a sourcefile's array of sentences, dictionary of words with respective counts, and arbitrary (but consistently) ordered list of words
+
+
+# returns a sourcefile's array of sentences, dictionary of words with respective counts, and arbitrary (but 
+# consistently) ordered list of words 
 def sentencify(sourcefile):
-	sentences = []
-	for line in open(sourcefile,'r'):
-		l1 = ''
-		for i in range(len(line)):
-			try:
-				if line[i] != '\n':
-					l1+=(line[i]) 
-			except UnicodeEncodeError:
-				pass
-		line_sent = l1.lower().split(".")
-		if len(sentences)>0:
-			if len(line_sent)>0:
-				sentences[-1]+=line_sent[0]
-			sentences+=line_sent[1:]
-		else:
-			sentences = line_sent
-	words = {}
-	for x in sentences:
-		for y in x.split(" "):
-			words[y] = words.get(y,0)+1
-	word_list = list(words)
-	word_list.sort()
-	while '' in sentences:
-		sentences.remove('')
-	return sentences, words, word_list
+    sentences = []
+    for line in open(sourcefile, 'r'):
+        l1 = ''
+        for i in range(len(line)):
+            try:
+                if line[i] != '\n':
+                    l1 += (line[i])
+            except UnicodeEncodeError:
+                pass
+        line_sent = l1.lower().split(".")
+        if len(sentences) > 0:
+            if len(line_sent) > 0:
+                sentences[-1] += line_sent[0]
+            sentences += line_sent[1:]
+        else:
+            sentences = line_sent
+    words = {}
+    for x in sentences:
+        for y in x.split(" "):
+            words[y] = words.get(y, 0) + 1
+    word_list = list(words)
+    word_list.sort()
+    while '' in sentences:
+        sentences.remove('')
+    return sentences, words, word_list
 
-#simple helper method that counts the number of occurences of element in list_
-def count(list_,element):
-	lc = list_.copy()
-	r= 0
-	while element in lc:
-		lc.remove(element)
-		r+=1
-	return r
 
-#represents each sentence as a vector related to its relative frequency of words in the word_list
+# simple helper method that counts the number of occurences of element in list_
+def count(list_, element):
+    lc = list_.copy()
+    r = 0
+    while element in lc:
+        lc.remove(element)
+        r += 1
+    return r
+
+
+# represents each sentence as a vector related to its relative frequency of words in the word_list
 def vectorize(sentence, words, word_list):
-	l = []
-	for word in word_list:
-		l.append(count(sentence.split(" "),word)*1.0/len(sentence.split(" "))*1/words.get(word,1)) #adds vector data on relative frequency of word
-	return l
+    l = []
+    for word in word_list:
+        l.append(count(sentence.split(" "), word) * 1.0 / len(sentence.split(" ")) * 1 / words.get(word,
+                                                                                                   1))  # adds vector data on relative frequency of word
+    return l
 
-sentences,words,word_list = sentencify('posts.txt')
+
+sentences, words, word_list = sentencify('posts.txt')
 sentence_d = {}
 for sentence in sentences:
-	sentence_d[sentence] = vectorize(sentence,words,word_list)
-tags = {} #blank tags dictionary (for user submitted tags)
+    sentence_d[sentence] = vectorize(sentence, words, word_list)
+tags = {}  # blank tags dictionary (for user submitted tags)
 print("A series of sentences will be presented to you.")
-print("On a scale from 0-100, indicate the 'positivity' of the statement, where 100 represents an extremely positive statement.")
-print("You will have to hit enter to submit each time. You will also have to hit enter an additional time when all sentences have been prompted.")
-#allows user to rate sentences
+
+print(
+    "On a scale from 0-100, indicate the 'positivity' of the statement, where 100 represents an extremely positive "
+    "statement.")
+
+print(
+    "You will have to hit enter to submit each time. You will also have to hit enter an additional time when all "
+    "sentences have been prompted.")
+
+# allows user to rate sentences
+
 with open('classify.txt') as f:
-	for a in f.readlines():
-		for sentence in sentences:
-			tags[sentence] = a
-l_vectors,l_tags = [],[]
+    for a in f.readlines():
+        for sentence in sentences:
+            tags[sentence] = a
+
+l_vectors, l_tags = [], []
+
 for sentence in sentences:
-	l_vectors.append(sentence_d[sentence])
-	l_tags.append(tags[sentence])
-l_tags,l_vectors = numpy.asarray(l_tags),numpy.asarray(l_vectors)
-print(str(l_vectors)+"vectors in training")
+    l_vectors.append(sentence_d[sentence])
+    l_tags.append(tags[sentence])
+
+l_tags, l_vectors = numpy.asarray(l_tags), numpy.asarray(l_vectors)
+print(str(l_vectors) + "vectors in training")
+
 train = svm.SVC()
-train.fit(l_vectors,l_tags)
-#forms our vectors and tags and submits them as appropriate into our virtual machine
-test_sent,_,_1 = sentencify('test.txt')
-#extracts sentences from test document
+train.fit(l_vectors, l_tags)
+
+# forms our vectors and tags and submits them as appropriate into our virtual machine
+
+test_sent, _, _1 = sentencify('test.txt')
+
+# extracts sentences from test document
+
 test_d = {}
+
 for sentence in test_sent:
-	test_d[sentence] = vectorize(sentence,words,word_list) # we use the dictionary and list ordering from the training data for consistency
-l,lname = [],[]
+    test_d[sentence] = vectorize(sentence, words,
+                                 word_list)  # we use the dictionary and list ordering from the training data for 
+    # consistency 
+
+l, lname = [], []
+
 for x in test_d:
-	lname.append(x)
-	l.append(test_d[x])
-x = train.predict(numpy.asarray(l))#prints our predicted ratings
+    lname.append(x)
+    l.append(test_d[x])
+x = train.predict(numpy.asarray(l))  # prints our predicted ratings
+
 print("PREDICTED POSITIVITY LEVELS\n for")
 for i in range(len(x)):
-	print(lname[i]+" : "+str(x[i]))
+    print(lname[i] + " : " + str(x[i]))
